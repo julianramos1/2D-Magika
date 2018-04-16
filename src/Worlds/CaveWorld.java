805af1d08670e462b43f011ae7c1aa173ec7928f
Josian Velez Ramos <josian.velez@upr.edu>
2018-04-16 19:46:40 -0400

    Add files via upload

---
+++ src/Worlds/CaveWorld.java
@@ -0,0 +1,20 @@
+package Worlds;
+import Game.Entities.Creatures.Player;
+import Main.Handler;
+
+/**
+ * Created by Elemental on 2/10/2017.
+ */
+public class CaveWorld extends BaseWorld{
+    private Handler handler;
+    private Player player;
+
+    public CaveWorld(Handler handler, String path, Player player) {
+        super(handler,path,player);
+        this.handler = handler;
+        this.player=player;
+
+    }
+
+
+}
