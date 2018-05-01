27041b676711fc1d93ed594e49915bf8fc68f9a4
Alejandro Ruiz <alejandroruiz2@upr.edu>
2018-04-30 21:28:39 -0400

    Changed Berry to Stick!
    Bush drops stick now, also bush bounds has been fixed.

--- src/Resources/Images.java
+++ src/Resources/Images.java
@@ -59,7 +59,7 @@ public class Images {
         SpriteSheet FireBallUpsheet = new SpriteSheet(Images.loadImage("/Sheets/FireBallUp.png"));
         SpriteSheet FireBallDownsheet = new SpriteSheet(Images.loadImage("/Sheets/FireBallDown.png"));
         SpriteSheet bushsheet = new SpriteSheet(Images.loadImage("/Sheets/Bushsheet.png"));
-        SpriteSheet berry = new SpriteSheet(Images.loadImage("/Sheets/berry.png"));
+        SpriteSheet stick = new SpriteSheet(Images.loadImage("/Sheets/Stick.png"));
 
 
 
@@ -209,8 +209,8 @@ public class Images {
             
             
             // Bush and trees array
-            bush[0] = bushsheet.crop(295, 140, 84, 80);//bush
-            bush[1] = berry.crop(0, 0, 64, 64);
+            bush[0] = bushsheet.crop(295, 140, 84, 80); // Bush
+            bush[1] = stick.crop(0, 0, 32, 32); // Stick
 
 
             //player anim
